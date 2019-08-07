2853d0676f586ac493e0f0da22443f285a914558
daniel Woodford <daniel.woodford.g@gmail.com>
2019-08-07 21:28:07 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,10 @@
 class Cat
   
   attr_reader :name 
-  attr_accessor :owner
+  attr_accessor :owner, :mood
   
   
-  def initialize(name, owner, mood )
+  def initialize(name, owner, mood)
     @name = name 
     @owner = owner 
     @mood = "nervous" 
