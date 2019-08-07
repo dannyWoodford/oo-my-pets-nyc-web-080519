c7445caf42bc02ddc82602ba3767860c95cd1d16
daniel Woodford <daniel.woodford.g@gmail.com>
2019-08-07 21:11:18 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,9 +2,13 @@ class Owner
   
   attr_reader :name, :species
   
+  
    def initialize(name)
     @name = name 
     @species = "human"
   end 
   
+    def say_species
+      "I am a #{species}"
+    end 
 end
